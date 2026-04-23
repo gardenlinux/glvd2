ce2d5897450b8a64e7600480faf1a8038ae2b384
Bernd Ritter <ritter@b1-systems.de>
2026-04-23 15:29:44 +0200

    removed panic() calls
    Signed-off-by: Bernd Ritter <ritter@b1-systems.de>
    On-behalf-of: SAP <b.ritter@sap.com>

--- internal/github/github.go
+++ internal/github/github.go
@@ -1,17 +1,18 @@
 package github
 
 import (
+	"errors"
 	"log/slog"
 	"os"
 
 	httpclient "github.com/gardenlinux/glvd2/internal/whttp"
 )
 
-func NewClient() *httpclient.HTTPClient {
+func NewClient() (*httpclient.HTTPClient, error) {
 	token := os.Getenv("GH_TOKEN")
 	if token == "" {
 		slog.With("client", "github").Error("GH_TOKEN is missing")
-		panic("GH_TOKEN is missing")
+		return nil, errors.New("Env GH_TOKEN is missing")
 	}
 
 	client := httpclient.NewClient()
@@ -19,5 +20,5 @@ func NewClient() *httpclient.HTTPClient {
 	client.Headers = append(client.Headers, httpclient.MakeHeader("Authorization", "Bearer "+token))
 	client.Headers = append(client.Headers, httpclient.MakeHeader("X-GitHub-Api-Version", "2026-03-10"))
 	slog.With("client", "github").Debug("new http client with auth token")
-	return client
+	return client, nil
 }
